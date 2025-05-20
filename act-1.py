bacd1a10818ce33648a992186c97060dd5c65811
Nihanth2025 <sk1441@gmail.com>
2025-05-20 16:46:57 +0530

    Add files via upload

---
+++ act-1.py
@@ -0,0 +1,18 @@
+a=22
+b=95
+c=32
+d=37
+
+epr= a+d * (b / c)
+
+print("Value of (a+d) * b / c = ",epr)
+
+
+n= "Nihanth"
+a=27
+
+if n== "Nihanth" and a>25 or n== "Nihitha" and a>25:
+    print("Welcome to club")
+
+else:
+    print("Good Bye!")
---
+++ act-2.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the numerator: "))
+b=int(input("Enter the denominator: "))
+
+if a%b ==0:
+    print(str(a)+" is divisible by "+str(b))
+
+else:
+    print(str(a)+" is not divisible by "+str(b))
